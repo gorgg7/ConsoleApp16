6d0042a14a1759284519e14feeb812f3bcf671bf
georg <georg@GORGG>
2025-02-26 22:49:12 +0200

    Add project files.

---
+++ ConsoleApp16/Migrations/20250226202755_InitialCreate.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ConsoleApp16.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    duration = table.Column<int>(type: "int", nullable: false),
+                    cname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                columns: table => new
+                {
+                    Departmentid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_departments", x => x.Departmentid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "instructors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    bonus = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    salary = table.Column<int>(type: "int", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    hourrate = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_instructors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                columns: table => new
+                {
+                    Studentid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Lname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    age = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_students", x => x.Studentid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "topic",
+                columns: table => new
+                {
+                    Topicid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    tname = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_topic", x => x.Topicid);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "courses");
+
+            migrationBuilder.DropTable(
+                name: "departments");
+
+            migrationBuilder.DropTable(
+                name: "instructors");
+
+            migrationBuilder.DropTable(
+                name: "students");
+
+            migrationBuilder.DropTable(
+                name: "topic");
+        }
+    }
+}
---
+++ ConsoleApp16/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class student
+{
+    public int Studentid { get; set; }
+    public string FName { get; set; }
+    public string Lname { get; set; }
+    public string address {  get; set; }    
+    public int age {  get; set; }
+
+}
+public class course
+{
+    public int Id { get; set; }
+    public int duration { get; set; }
+    public string cname { get; set; }
+    public string description { get; set; }
+
+}
+public class instructor
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string bonus { get; set; }
+    public int salary { get; set; }
+    public string address { get; set; }
+    public int hourrate { get; set; }
+
+}
+public class department
+{
+    public int Departmentid { get; set; }
+    public string DName { get; set; }
+}
+public class topic
+{
+    public int Topicid { get; set; }
+    public string tname { get; set; }
+}
+public class iti : DbContext
+{
+    public DbSet<student> students { get; set; }
+    public DbSet<course> courses { get; set; }
+    public DbSet<instructor> instructors { get; set; }
+    public DbSet<department> departments { get; set; }
+    public DbSet<topic> topic { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("server=GORGG;database=itief;trusted_connection=true;trustServerCertificate=true");
+    }
+}
+class program {
+public static void Main(string[] args) { }
+
+
+}
+
+
